a92228734f93bcafd1c1e7d44bd7005126c0f0af
John Bonini <bonini.physics@gmail.com>
2025-02-16 17:12:33 -0500

    1.10 run

--- src/enzyme_test.jl
+++ src/enzyme_test.jl
@@ -132,16 +132,19 @@ function diagprodthree!(res, A, B, C)
     nothing
 end
 
-function fe_diagprodthree!(res, A, B, C)
+function truefe_diagprodthree!(res, A, B, C)
     # Mismatched activity!
     # will error if any single loop is changed
-    # foreach(axes(A, 1)) do i
-    #     foreach(axes(B, 1)) do j
-    #         foreach(axes(C, 1)) do k
-    #             res[i] += A[i, j] * B[j, k] * C[k, i]
-    #         end
-    #     end
-    # end
+    foreach(axes(A, 1)) do i
+        foreach(axes(B, 1)) do j
+            foreach(axes(C, 1)) do k
+                res[i] += A[i, j] * B[j, k] * C[k, i]
+            end
+        end
+    end
+end
+
+function fe_diagprodthree!(res, A, B, C)
     for i in axes(A, 1)
         for j in axes(B, 1)
             for k in axes(C, 1)
@@ -161,14 +164,17 @@ function _obj_inner!(ik, bp, mats, storage)
     end
 end
 
-function fe_obj_inner!(ik, bp, mats, storage)
+function truefe_obj_inner!(ik, bp, mats, storage)
     # Mismatched activity!
-    # foreach(enumerate(bp)) do (ib, bk)
-    #     fe_diagprodthree!(storage.diags,
-    #                    storage.M[ik],
-    #                    mats[ik][ib],
-    #                    adjoint(storage.M[bk]))
-    # end
+    foreach(enumerate(bp)) do (ib, bk)
+        fe_diagprodthree!(storage.diags,
+                       storage.M[ik],
+                       mats[ik][ib],
+                       adjoint(storage.M[bk]))
+    end
+end
+
+function fe_obj_inner!(ik, bp, mats, storage)
     for (ib, bk) in enumerate(bp)
         fe_diagprodthree!(storage.diags,
                        storage.M[ik],
@@ -199,22 +205,53 @@ function ∇objective!(∂AAA, AAA, C, storage, ∂storage)
     nothing
 end
 
-function fe_objective!(AAA, C, storage)
+function truefe_objective!(AAA, C, storage)
+    fe_each_m_to_AAmat!(storage.M, AAA)
+    # ## Mismatched activity! + doubles primal allocations unles res is Ref
+    res = 0.0
+    foreach(enumerate(C.bps)) do (ik, bp)
+        fill!(storage.diags, 0.0)
+        fe_obj_inner!(ik, bp, C.mats, storage)
+        res += sum(x->1-abs2(x), storage.diags)
+    end
+    res
+end
+
+function ∇truefe_objective!(∂AAA, AAA, C, storage, ∂storage)
+    Enzyme.make_zero!(∂AAA)
+    Enzyme.make_zero!(∂storage)
+    Enzyme.autodiff(Enzyme.Reverse,
+        truefe_objective!, Enzyme.Active,
+        Enzyme.Duplicated(AAA, ∂AAA),
+        Enzyme.Const(C),
+        Enzyme.Duplicated(storage, ∂storage))
+    nothing
+end
+
+function sumfe_objective!(AAA, C, storage)
     fe_each_m_to_AAmat!(storage.M, AAA)
     ## Mismatched activity!
-    # sum(enumerate(C.bps)) do (ik, bp)
-    #     fill!(storage.diags, 0.0)
-    #     fe_obj_inner!(ik, bp, C.mats, storage)
-    #     sum(x->1-abs2(x), storage.diags)
-    # end
+    sum(enumerate(C.bps)) do (ik, bp)
+        fill!(storage.diags, 0.0)
+        truefe_obj_inner!(ik, bp, C.mats, storage)
+        sum(x->1-abs2(x), storage.diags)
+    end
+end
+
+function ∇sumfe_objective!(∂AAA, AAA, C, storage, ∂storage)
+    Enzyme.make_zero!(∂AAA)
+    Enzyme.make_zero!(∂storage)
+    Enzyme.autodiff(Enzyme.Reverse,
+        sumfe_objective!, Enzyme.Active,
+        Enzyme.Duplicated(AAA, ∂AAA),
+        Enzyme.Const(C),
+        Enzyme.Duplicated(storage, ∂storage))
+    nothing
+end
+
+function fe_objective!(AAA, C, storage)
+    fe_each_m_to_AAmat!(storage.M, AAA)
     res = 0.0
-    ## Mismatched activity! + doubles primal allocations unles res is Ref
-    # foreach(enumerate(C.bps)) do (ik, bp)
-    #     fill!(storage.diags, 0.0)
-    #     fe_obj_inner!(ik, bp, C.mats, storage)
-    #     res += sum(x->1-abs2(x), storage.diags)
-    # end
-    ## Runs
     for (ik, bp) in enumerate(C.bps)
         fill!(storage.diags, 0.0)
         fe_obj_inner!(ik, bp, C.mats, storage)
@@ -264,29 +301,71 @@ function init_params(; seed, lnn, nk, nbs, nd, lovary)
     (;∂AAA, AAA, C, storage, ∂storage)
 end
 
-function run_them!(time_dat; kwargs...)
+function run_complicated!(time_dat; kwargs...)
     (; ∂AAA, AAA, C, storage, ∂storage) = init_params(; kwargs...)
 
     (;foreach_times, for_times) = time_dat
 
     push!(for_times.primal.comp, (@timed objective!(AAA, C, storage)))
     push!(for_times.primal.run, (@timed objective!(AAA, C, storage)))
-    push!(for_times.grad.comp, (@timed ∇objective!(∂AAA, AAA, C, storage, ∂storage)))
-    push!(for_times.grad.run, (@timed ∇objective!(∂AAA, AAA, C, storage, ∂storage)))
+    try
+        push!(for_times.grad.comp, (@timed ∇objective!(∂AAA, AAA, C, storage, ∂storage)))
+        push!(for_times.grad.run, (@timed ∇objective!(∂AAA, AAA, C, storage, ∂storage)))
+    catch error
+        println("for, gradient failed for $(kwargs) with $(error)")
+    end
 
     push!(foreach_times.primal.comp, (@timed fe_objective!(AAA, C, storage)))
     push!(foreach_times.primal.run, (@timed fe_objective!(AAA, C, storage)))
-    push!(foreach_times.grad.comp, (@timed ∇fe_objective!(∂AAA, AAA, C, storage, ∂storage)))
-    push!(foreach_times.grad.run, (@timed ∇fe_objective!(∂AAA, AAA, C, storage, ∂storage)))
+    try
+        push!(foreach_times.grad.comp, (@timed ∇fe_objective!(∂AAA, AAA, C, storage, ∂storage)))
+        push!(foreach_times.grad.run, (@timed ∇fe_objective!(∂AAA, AAA, C, storage, ∂storage)))
+    catch error
+        println("for, gradient failed for $(kwargs) with $(error)")
+    end
 
     nothing
 end
 
-TimedRes{T} = @NamedTuple{value::T,
-                          time::Float64,
-                          bytes::Int64,
-                          gctime::Float64,
-                          gcstats::Base.GC_Diff}
+function run_failing(; kwargs...)
+    (; ∂AAA, AAA, C, storage, ∂storage) = init_params(; kwargs...)
+
+    println("foreach/sum cases which fail:")
+    @time "all foreach primal" truefe_objective!(AAA, C, storage)
+    @time "all foreach primal" truefe_objective!(AAA, C, storage)
+    try
+        @time "all foreach grad" ∇truefe_objective!(∂AAA, AAA, C, storage, ∂storage)
+        @time "all foreach grad" ∇truefe_objective!(∂AAA, AAA, C, storage, ∂storage)
+    catch error
+        println("all foreach , gradient failed for $(kwargs) with $(error)")
+        println("all foreach , gradient failed for $(kwargs) with $(error)")
+    end
+    @time "sum + rest foreach primal" sumfe_objective!(AAA, C, storage)
+    try
+        @time "sum + rest foreach grad" ∇sumfe_objective!(∂AAA, AAA, C, storage, ∂storage)
+        @time "sum + rest foreach grad" ∇sumfe_objective!(∂AAA, AAA, C, storage, ∂storage)
+    catch error
+        println("sum + rest foreach, gradient failed for $(kwargs) with $(error)")
+    end
+end
+
+
+# Julia version 1.10 vs 1.11 have different time / timed macro behavior
+TimedRes{T} = VERSION.minor == UInt32(11) ? @NamedTuple{ # Julia 1.11
+    value::T,
+    time::Float64,
+    bytes::Int64,
+    gctime::Float64,
+    gcstats::Base.GC_Diff,
+    lock_conflicts::Int64,
+    compile_time::Float64,
+    recompile_time::Float64} : @NamedTuple{ # Julia 1.10
+        value::T,
+        time::Float64,
+        bytes::Int64,
+        gctime::Float64,
+        gcstats::Base.GC_Diff}
+
 CompRunRes{T} = @NamedTuple{comp::TimedRes{T}, run::TimedRes{T}}
 CompRunRes{T}() where {T} = (;comp=TimedRes{T}[], run=TimedRes{T}[])
 PrimalGradRes{T, TG} = @NamedTuple{primal::CompRunRes{T}, grad::CompRunRes{TG}}
@@ -294,12 +373,17 @@ PrimalGradRes{T, TG}() where {T, TG} = (;primal=CompRunRes{T}(), grad=CompRunRes
 
 function show_one(timed_res; msg=nothing)
     (; time, gcstats) = timed_res
-    Base.time_print(Base.stdout, time*1e9, gcstats.allocd, gcstats.total_time,
-                    Base.gc_alloc_count(gcstats), 0, 0, true; msg=msg)
+    (VERSION.minor == UInt32(11) ? Base.time_print(
+        Base.stdout, time*1e9, gcstats.allocd, gcstats.total_time,
+        Base.gc_alloc_count(gcstats), timed_res.lock_conflicts, timed_res.compile_time * 1e9, timed_res.recompile_time*1e9, true;
+        msg=msg) : Base.time_print(
+            Base.stdout, time*1e9, gcstats.allocd, gcstats.total_time,
+            Base.gc_alloc_count(gcstats), 0, 0, true; msg=msg))
 end
 
 function show_all(time_dat)
-    println(Pkg.status())
+    println("Complicated cases which succeeded")
+    println("Here foreach is only used where it does not cause activity errors")
     println("for, primal run")
     foreach(show_one, time_dat.for_times.primal.run)
     println("foreach, primal run")
@@ -328,8 +412,8 @@ end
 
 function simpleobjective!(A)
     res = 0.0
-    for v in A
-        res += v
+    for (i, v) in enumerate(A)
+        res += 1 - abs2(v)*i
     end
     res
 end
@@ -344,17 +428,10 @@ end
 
 function fe_simpleobjective!(A)
     res = 0.0
-    # enzyme grad allocates, but works
     foreach(enumerate(A)) do (i, v)
-        res += v
+        res += 1 - abs2(v) * i
     end
     res
-    # no grad allocation, and works
-    # sum(A)
-    # also doesn't allocate, and works
-    # sum(enumerate(A)) do (i,v)
-    #     1-abs2(v) * i
-    # end
 end
 
 function ∇fe_simpleobjective!(∂A, A)
@@ -365,25 +442,56 @@ function ∇fe_simpleobjective!(∂A, A)
     nothing
 end
 
-function main(lnns=2:3:20)
+function sum_simpleobjective!(A)
+    sum(enumerate(A)) do (i,v)
+        1 - abs2(v) * i
+    end
+end
+
+function ∇sum_simpleobjective!(∂A, A)
+    Enzyme.make_zero!(∂A)
+    Enzyme.autodiff(Enzyme.Reverse,
+        sum_simpleobjective!, Enzyme.Active,
+        Enzyme.Duplicated(A, ∂A))
+    nothing
+end
+
+function run_simple()
     simple_vec = rand(10)
     ∂simple_vec = Enzyme.make_zero(simple_vec)
+    println("Simple foreach/sum cases which work:")
     @time  "simple run  for    " simpleobjective!(simple_vec)
     @time  "simple run  for    " simpleobjective!(simple_vec)
     @time  "simple run  foreach" fe_simpleobjective!(simple_vec)
     @time  "simple run  foreach" fe_simpleobjective!(simple_vec)
+    @time  "simple run  sum    " sum_simpleobjective!(simple_vec)
+    @time  "simple run  sum    " sum_simpleobjective!(simple_vec)
     @time  "simple grad for    " ∇simpleobjective!(∂simple_vec, simple_vec)
     @time  "simple grad for    " ∇simpleobjective!(∂simple_vec, simple_vec)
     @time  "simple grad foreach" ∇fe_simpleobjective!(∂simple_vec, simple_vec)
     @time  "simple grad foreach" ∇fe_simpleobjective!(∂simple_vec, simple_vec)
-    println("End simple cases")
+    @time  "simple grad sum    " ∇sum_simpleobjective!(∂simple_vec, simple_vec)
+    @time  "simple grad sum    " ∇sum_simpleobjective!(∂simple_vec, simple_vec)
+end
+
+function main(lnns=15:20)
+    println("Julia: v$(VERSION)")
+    Pkg.status()
+    println()
 
     time_dat = (;foreach_times = PrimalGradRes{Float64, Nothing}(),
                 for_times = PrimalGradRes{Float64, Nothing}(),)
     foreach(lnns) do lnn
-        run_them!(time_dat; seed = 123, lnn = lnn, nk = 20, nbs = 5, nd = 5, lovary = 0:9)
+        println(lnn)
+        run_complicated!(time_dat; seed = 123, lnn = lnn, nk = 20, nbs = 5, nd = 5, lovary = 0:9)
     end
     show_all(time_dat)
+    println()
+
+    run_simple()
+    println()
+
+    run_failing(; seed = 123, lnn = 3, nk = 20, nbs = 5, nd = 5, lovary = 0:9)
     time_dat
 end
 
